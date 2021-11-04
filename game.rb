fba15065d9897db9fc6a82ce6705cbe2c1848b37
ATDillon <vieridin@gmail.com>
2021-11-03 17:32:17 -0700

    Add boolean methods, update code_checker
    code_checker now reduces into a single array containing the hint for the
    secret code. Boolean methods same_char? and same_amount? check the
    individual characters of the guess against the secret code. A method for
    printing the return value of code_checker also added.

--- game.rb
+++ game.rb
@@ -2,15 +2,49 @@
 class Game
   attr_reader :code
 
-  def initialize(player_one:, player_two:, code: 'rbyg')
+  def initialize(player_one:, player_two:, code: ['r', 'b', 'y', 'o'])
     @player_one = player_one
     @player_two = player_two
     @code = code
+  end
+
+  private
+
+  def same_amount?(guess, char, index)
+    if (guess.count(char) == code.count(char)) || (guess[0..index].count(char) == code[0..index].count(char))
+      return true
+    end
+
+    false
   end
 
   def code_same?(guess)
     return true if guess == code
   end
 
-  def code_checker(guess); end
+  def char_same?(char, index)
+    return true if char == code[index]
+
+    false
+  end
+
+  def code_checker(guess)
+    guess.each_with_index.reduce([]) do |hint, (char, index)|
+      if char_same?(char, index)
+        hint.push('+')
+      elsif same_amount?(guess, char, index)
+        hint.push('~')
+      else
+        hint.push('-')
+      end
+    end
+  end
+
+  public
+
+  def print_hint(guess)
+    puts code_checker(guess).join(' ')
+  end
 end
+
+Game.new(player_one: 'sally', player_two: 'jane').print_hint(['b', 'r', 'y', 'y'])
