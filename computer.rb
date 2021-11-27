2188617e5ecce3b9887bc62b30172e2f84825644
ATDillon <vieridin@gmail.com>
2021-11-26 18:27:49 -0800

    Add instance variable options to computer.rb
    Adds 'options' and accessor method. Fixes a bug in method 'combos'.

--- computer.rb
+++ computer.rb
@@ -5,18 +5,19 @@ class Computer
   private
 
   attr_reader :colors
-  attr_accessor :guess
+  attr_accessor :guess, :options
 
   def initialize(name: 'COM')
     @name = name
     @colors = %w[r b g y o p]
     @guess = []
+    @options = combos(4)
   end
 
   def combos(count)
     result = colors
     (count - 1).times do
-      result = options.reduce([]) do |a, color|
+      result = colors.reduce([]) do |a, color|
         a + result.map { |next_color| [color] + [next_color] }.each(&:flatten!)
       end
     end
